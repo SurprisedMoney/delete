9e99c379e2afccd9c4cb78bfb968ec6a47a5deb7
SurprisedMoney <52398558+SurprisedMoney@users.noreply.github.com>
2021-09-16 09:43:13 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# d={}
+# for i in range(26):
+#     d[chr(i+ord("A"))]=chr((i+13)%26+ord("A"))
+# for c in "Python":
+#     print(d.get(c,c),end='')
+# try:
+#     n = 0
+#     n = input("qingshuruyigeshuzi:")
+#     def pow10(n):
+#         return n**10
+#     pow(n)
+# except:
+#     print("error")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
